816d51066e664af70d86007c580c759f3680e5a6
hukusann <147576778+hukusann@users.noreply.github.com>
2023-10-19 22:49:17 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,83 @@
-const kanjiList = [
-    { kanji: "山", answer: "やま" },
-    { kanji: "川", answer: "かわ" },
-    { kanji: "木", answer: "き" },
-    { kanji: "花", answer: "はな" },
-    { kanji: "太陽", answer: "たいよう" },
-    { kanji: "雨", answer: "あめ" },
-    { kanji: "犬", answer: "いぬ" },
-    { kanji: "猫", answer: "ねこ" },
-    { kanji: "魚", answer: "さかな" },
-    { kanji: "鳥", answer: "とり" }
-];
+function startGame(level) {
+    // レベルに応じて問題をロードおよび表示
+    const kanjiList = getKanjiForLevel(level);
+    // ここで、選択したレベルに基づいて漢字データを取得します。
 
-let currentQuestionIndex = 0;
+    // HTML要素の取得
+    const container = document.querySelector('.container');
+    container.innerHTML = ''; // 以前のコンテンツをクリア
 
-const targetWordElement = document.getElementById("target-word");
-const guessInput = document.getElementById("guess-input");
-const checkButton = document.getElementById("check-button");
-const resultMessage = document.getElementById("result");
-const nextButton = document.getElementById("next-button");
+    // 問題の表示
+    const wordContainer = document.createElement('div');
+    wordContainer.id = 'word-container';
+    wordContainer.innerHTML = '<span id="target-word"></span>';
+    container.appendChild(wordContainer);
 
-function initializeGame() {
-    targetWordElement.textContent = kanjiList[currentQuestionIndex].kanji;
-    resultMessage.textContent = "";
-    guessInput.value = "";
-}
-initializeGame();
-checkButton.addEventListener("click", () => {
-    const userGuess = guessInput.value;
-    const correctAnswer = kanjiList[currentQuestionIndex].answer;
+    const guessInput = document.createElement('input');
+    guessInput.type = 'text';
+    guessInput.id = 'guess-input';
+    guessInput.placeholder = '漢字を入力';
+    container.appendChild(guessInput);
 
-    if (userGuess === correctAnswer) {
-        resultMessage.textContent = "正解です！";
+    const checkButton = document.createElement('button');
+    checkButton.id = 'check-button';
+    checkButton.textContent = '確認';
+    container.appendChild(checkButton);
 
-        // 正解の場合、3秒後に次の問題に進む
-        setTimeout(() => {
-            currentQuestionIndex++;
-            if (currentQuestionIndex < kanjiList.length) {
-                initializeGame();
-                nextButton.disabled = true;
-                checkButton.disabled = false;
-            } else {
-                resultMessage.textContent = "全問正解おめでとうございます！";
-                nextButton.disabled = true;
-                checkButton.disabled = true;
-            }
-        }, 2000); // 2秒後に次の問題に進む
-    } else {
-        resultMessage.textContent = `不正解です。正解は、 ${correctAnswer} です。`;
+    const resultMessage = document.createElement('p');
+    resultMessage.id = 'result';
+    container.appendChild(resultMessage);
+
+    // ゲームの初期化
+    let currentQuestionIndex = 0;
+    initializeGame();
+
+    checkButton.addEventListener('click', () => {
+        const userGuess = guessInput.value;
+        const correctAnswer = kanjiList[currentQuestionIndex].answer;
+
+        if (userGuess === correctAnswer) {
+            resultMessage.textContent = '正解です！';
+
+            setTimeout(() => {
+                currentQuestionIndex++;
+                if (currentQuestionIndex < kanjiList.length) {
+                    initializeGame();
+                } else {
+                    resultMessage.textContent = '全問正解おめでとうございます！';
+                    checkButton.disabled = true;
+                }
+            }, 3000);
+        } else {
+            resultMessage.textContent = `不正解です。正解は ${correctAnswer} です。`;
+        }
+    });
+
+    function initializeGame() {
+        const targetWordElement = document.getElementById('target-word');
+        targetWordElement.textContent = kanjiList[currentQuestionIndex].kanji;
+        resultMessage.textContent = '';
+        guessInput.value = '';
+        checkButton.disabled = false;
     }
-})
+}
+
+// レベルごとの漢字データを取得する関数
+function getKanjiForLevel(level) {
+    // レベルに応じて漢字データを取得または生成します。
+    // この部分はゲームのデータベースや外部データソースからデータを取得するロジックに置き換えることができます。
+    // 以下は仮想的な例です。
+    const kanjiData = {
+        1: [
+            { kanji: '山', answer: 'やま' },
+            { kanji: '川', answer: 'かわ' },
+            // 他の漢字データ
+        ],
+        2: [
+            // レベル2の漢字データ
+        ],
+        // 他のレベルの漢字データ
+    };
+
+    return kanjiData[level] || [];
+}
