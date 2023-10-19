50f654b82fc4a0a495248efd6d1f453a2b4eafd7
hukusann <147576778+hukusann@users.noreply.github.com>
2023-10-19 20:54:21 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const kanjiList = [
+    { kanji: "山", answer: "やま" },
+    { kanji: "川", answer: "かわ" },
+    { kanji: "木", answer: "き" },
+    { kanji: "花", answer: "はな" },
+    { kanji: "太陽", answer: "たいよう" },
+    { kanji: "雨", answer: "あめ" },
+    { kanji: "犬", answer: "いぬ" },
+    { kanji: "猫", answer: "ねこ" },
+    { kanji: "魚", answer: "さかな" },
+    { kanji: "鳥", answer: "とり" }
+];
+
+let currentQuestionIndex = 0;
+
+const targetWordElement = document.getElementById("target-word");
+const guessInput = document.getElementById("guess-input");
+const checkButton = document.getElementById("check-button");
+const resultMessage = document.getElementById("result");
+const nextButton = document.getElementById("next-button");
+
+function initializeGame() {
+    targetWordElement.textContent = kanjiList[currentQuestionIndex].kanji;
+    resultMessage.textContent = "";
+    guessInput.value = "";
+}
+initializeGame();
+checkButton.addEventListener("click", () => {
+    const userGuess = guessInput.value;
+    const correctAnswer = kanjiList[currentQuestionIndex].answer;
+
+    if (userGuess === correctAnswer) {
+        resultMessage.textContent = "正解です！";
+
+        // 正解の場合、3秒後に次の問題に進む
+        setTimeout(() => {
+            currentQuestionIndex++;
+            if (currentQuestionIndex < kanjiList.length) {
+                initializeGame();
+                nextButton.disabled = true;
+                checkButton.disabled = false;
+            } else {
+                resultMessage.textContent = "全問正解おめでとうございます！";
+                nextButton.disabled = true;
+                checkButton.disabled = true;
+            }
+        }, 2000); // 2秒後に次の問題に進む
+    } else {
+        resultMessage.textContent = "不正解です。もう一度挑戦してください.";
+    }
+});
