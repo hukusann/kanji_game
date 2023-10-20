cc179d5e719b166551ac790708bc052e66f8365e
hukusann <147576778+hukusann@users.noreply.github.com>
2023-10-20 15:22:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+let currentQuestionIndex = 0;
+let kanjiList = []; // 問題リストを追加
+
 function startGame(level) {
     // レベルに応じて問題をロードおよび表示
     const kanjiList = getKanjiForLevel(level);
-    // ここで、選択したレベルに基づいて漢字データを取得します。
 
+    shuffleArray(kanjiList);
     // HTML要素の取得
     const container = document.querySelector('.container');
     container.innerHTML = ''; // 以前のコンテンツをクリア
+
 
     // 問題の表示
     const wordContainer = document.createElement('div');
@@ -46,6 +50,10 @@ function startGame(level) {
                 } else {
                     resultMessage.textContent = '全問正解おめでとうございます！';
                     checkButton.disabled = true;
+                    initializeGame();
+                
+                // 問題番号を更新
+                questionNumber.textContent = currentQuestionIndex + 1;
                 }
             }, 3000);
         } else {
@@ -62,6 +70,16 @@ function startGame(level) {
     }
 }
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+// 問題番号を更新
+questionNumber.textContent = currentQuestionIndex + 1;
+    
+
 // レベルごとの漢字データを取得する関数
 function getKanjiForLevel(level) {
     // レベルに応じて漢字データを取得または生成します。
@@ -71,6 +89,14 @@ function getKanjiForLevel(level) {
         1: [
             { kanji: '山', answer: 'やま' },
             { kanji: '川', answer: 'かわ' },
+            { kanji: "海", answer: "うみ" },
+            { kanji: "中", answer: "なか" },
+            { kanji: "算数", answer: "さんすう" },
+            { kanji: "米", answer: "こめ" },
+            { kanji: "音楽", answer: "" },
+            { kanji: "母", answer: "はは" },
+            { kanji: "人物", answer: "じんぶつ" },
+            { kanji: "分野", answer: "ぶんや" },
             // 他の漢字データ
         ],
         2: [
